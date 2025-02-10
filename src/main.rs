00fd37960dc56b4f5a53206af908fd246879f4fe
Bruno <brmcerqueira@gmail.com>
2025-02-10 19:59:47 -0300

    Adjust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod query_engine;
 mod repository;
-use actix_web::{get, put, web, App, HttpResponse, HttpServer, Responder};
+use std::{sync::mpsc::{self, channel, Sender}, thread};
+
+use actix_web::{get, put, rt::task, web, App, HttpResponse, HttpServer, Responder};
 use query_engine::QueryEngine;
 use repository::Repository;
 use serde::{Deserialize, Serialize};
@@ -9,6 +11,7 @@ use v8::{new_default_platform, HandleScope, Isolate};
 
 struct AppState {
     repository: Repository,
+    query_engine: QueryEngine
 }
 
 #[derive(Deserialize, Serialize)]
@@ -36,22 +39,20 @@ async fn collection_create(
     return HttpResponse::Ok().json(CollectionCreate { id });
 }
 
+#[get("/query/{name}")]
+async fn query(
+    path: web::Path<String>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    data.query_engine.call.send(path.into_inner()).unwrap();
+    return HttpResponse::Ok();
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let platform = new_default_platform(0, false).make_shared();
-
-    v8::V8::initialize_platform(platform);
-    v8::V8::initialize();
-
-    let isolate = &mut Isolate::new(Default::default());
-    let mut handle_scope = HandleScope::new(isolate);
-
-    let query_engine = &mut QueryEngine::new(&mut handle_scope);
-
-    query_engine.call("name");
-
     let app_state = web::Data::new(AppState {
         repository: Repository::new(),
+        query_engine: QueryEngine::new("./script.js".to_string())
     });
 
     HttpServer::new(move || {
@@ -59,6 +60,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(app_state.clone())
             .service(collection_get)
             .service(collection_create)
+            .service(query)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/query_engine.rs
+++ src/query_engine.rs
@@ -1,73 +1,73 @@
-use std::fs;
+use std::{fs, sync::mpsc::{self, Sender}, thread};
 
 use v8::{
-    Context, ContextScope, FunctionCallbackArguments, FunctionTemplate,
-    HandleScope, Local, ObjectTemplate, ReturnValue, Script
+    new_default_platform, Context, ContextOptions, ContextScope, Function, FunctionCallbackArguments, FunctionTemplate, HandleScope, Isolate, Local, ObjectTemplate, ReturnValue, Script, V8::{initialize, initialize_platform}
 };
 
-pub struct QueryEngine<'s, 'i> {
-    context: Local<'s, Context>,
-    context_scope: ContextScope<'i, HandleScope<'s>>,
+pub struct QueryEngine {
+    pub call: Sender<String>
 }
 
-impl<'s, 'i> QueryEngine<'s, 'i>
-where
-    's: 'i,
-{
-    pub fn new(isolate_scope: &'i mut HandleScope<'s, ()>) -> Self {
-        let global = ObjectTemplate::new(isolate_scope);
+impl QueryEngine {
+    pub fn new(path: String) -> Self {
+        let (call, rx) = mpsc::channel::<String>();
+        
+        thread::spawn(|| {
+            let platform = new_default_platform(0, false).make_shared();
 
-        global.set(
-            v8::String::new(isolate_scope, "filter").unwrap().into(),
-            FunctionTemplate::new(isolate_scope, QueryEngine::filter).into(),
-        );
-
-        let context = Context::new(
-            isolate_scope,
-            v8::ContextOptions {
-                global_template: Some(global),
-                ..Default::default()
-            },
-        );
-
-        let mut context_scope = ContextScope::new(isolate_scope, context);
-
-        let code = fs::read_to_string("./script.js")
-            .expect("failed to convert Rust string to javascript string");
-
-        let code = v8::String::new(&mut context_scope, &code).unwrap();
-
-        println!(
-            "javascript code: {}",
-            code.to_rust_string_lossy(&mut context_scope)
-        );
-
-        let script = Script::compile(&mut context_scope, code, None).unwrap();
-
-        script.run(&mut context_scope);
+            initialize_platform(platform);
+            initialize();
+        
+            let isolate = &mut Isolate::new(Default::default());
+            let mut isolate_scope = HandleScope::new(isolate);
+    
+            let global_template = ObjectTemplate::new(&mut isolate_scope);
+    
+            global_template.set(
+                v8::String::new(&mut isolate_scope, "filter").unwrap().into(),
+                FunctionTemplate::new(&mut isolate_scope, QueryEngine::filter).into(),
+            );
+    
+            let context = Context::new(
+                &mut isolate_scope,
+                ContextOptions {
+                    global_template: Some(global_template),
+                    ..Default::default()
+                },
+            );
+    
+            let mut context_scope = ContextScope::new(&mut isolate_scope, context);
+    
+            let code = fs::read_to_string(path)
+                .expect("failed to read script path");
+    
+            let code = v8::String::new(&mut context_scope, &code).unwrap();
+    
+            let script = Script::compile(&mut context_scope, code, None).unwrap();
+    
+            script.run(&mut context_scope);
+    
+            let global = context.global(&mut context_scope);
+    
+            for name in rx {
+                let function_name_string = v8::String::new(&mut context_scope, &name)
+                    .expect("failed to convert Rust string to javascript string");
+        
+                let function = global
+                    .get(&mut context_scope, function_name_string.into())
+                    .expect(&*format!("could not find function {}", name));
+        
+                let function: Local<Function> = Local::cast(function);
+              
+                let recv = v8::Integer::new(&mut context_scope, 2).into();
+        
+                function.call(&mut context_scope, recv, &[]);
+            }  
+        });
 
         return QueryEngine {
-            context,
-            context_scope,
+            call
         };
-    }
-
-    pub fn call(&mut self, name: &str) {
-        let global =  self.context.global(&mut self.context_scope);
-  
-        // getting the javascript function whose name is function_name parameter
-        let function_name_string = v8::String::new(&mut self.context_scope, &name)
-            .expect("failed to convert Rust string to javascript string");
-
-        let function = global
-            .get(&mut self.context_scope, function_name_string.into())
-            .expect(&*format!("could not find function {}", name));
-
-        let function: v8::Local<v8::Function> = v8::Local::cast(function);
-        // call that function with arguments
-        let recv = v8::Integer::new(&mut self.context_scope, 2).into();
-
-        function.call(&mut self.context_scope, recv, &[]);
     }
 
     fn filter(scope: &mut HandleScope, args: FunctionCallbackArguments, mut _retval: ReturnValue) {
