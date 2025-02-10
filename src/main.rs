6143c6e3d6eebaf1e016ac9f75942f56a6b8976d
Bruno <brmcerqueira@gmail.com>
2025-02-10 17:28:44 -0300

    Adjust

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,57 @@
-mod repository;
 mod query_engine;
+mod repository;
 use actix_web::{get, put, web, App, HttpResponse, HttpServer, Responder};
 use query_engine::QueryEngine;
 use repository::Repository;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use v8::{new_default_platform, HandleScope, Isolate};
 
 struct AppState {
-    repository: Repository
+    repository: Repository,
 }
 
 #[derive(Deserialize, Serialize)]
 struct CollectionCreate {
-    id: u64
+    id: u64,
 }
 
 #[get("/collection/{name}/{id}")]
-async fn collection_get(path: web::Path<(String, u64)>, data: web::Data<AppState>) -> impl Responder {
+async fn collection_get(
+    path: web::Path<(String, u64)>,
+    data: web::Data<AppState>,
+) -> impl Responder {
     let (name, id) = path.into_inner();
     let body = data.repository.get(name, id);
     return HttpResponse::Ok().body(body);
 }
 
 #[put("/collection/{name}")]
-async fn collection_create(json: web::Json<Value>, path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+async fn collection_create(
+    json: web::Json<Value>,
+    path: web::Path<String>,
+    data: web::Data<AppState>,
+) -> impl Responder {
     let id = data.repository.create(path.into_inner(), json.into_inner());
-    return HttpResponse::Ok().json(CollectionCreate {
-        id
-    });
+    return HttpResponse::Ok().json(CollectionCreate { id });
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let query_engine = &mut QueryEngine::new();
+    let platform = new_default_platform(0, false).make_shared();
+
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+
+    let isolate = &mut Isolate::new(Default::default());
+    let mut handle_scope = HandleScope::new(isolate);
+
+    let query_engine = &mut QueryEngine::new(&mut handle_scope);
+
+    query_engine.call("name");
 
     let app_state = web::Data::new(AppState {
-        repository: Repository::new(), 
+        repository: Repository::new(),
     });
 
     HttpServer::new(move || {
--- src/query_engine.rs
+++ src/query_engine.rs
@@ -1,64 +1,73 @@
 use std::fs;
 
 use v8::{
-    new_default_platform, Context, ContextScope, FunctionCallbackArguments, FunctionTemplate, HandleScope, Isolate, ObjectTemplate, ReturnValue, Script
+    new_default_platform, Context, ContextScope, FunctionCallbackArguments, FunctionTemplate,
+    HandleScope, Isolate, Local, Object, ObjectTemplate, ReturnValue, Script,
 };
 
-pub struct QueryEngine {
+pub struct QueryEngine<'s, 'i> {
+    context: Local<'s, Context>,
+    context_scope: ContextScope<'i, HandleScope<'s>>,
 }
 
-impl QueryEngine {
-    pub fn new() -> Self {
-        let platform = new_default_platform(0, false).make_shared();
+impl<'s, 'i> QueryEngine<'s, 'i>
+where
+    's: 'i,
+{
+    pub fn new(isolate_scope: &'i mut HandleScope<'s, ()>) -> Self {
+        let global = ObjectTemplate::new(isolate_scope);
 
-        v8::V8::initialize_platform(platform);
-        v8::V8::initialize();
-
-        let isolate = &mut Isolate::new(Default::default());
-        let mut handle_scope = HandleScope::new(isolate);
-
-        let global = ObjectTemplate::new(&mut handle_scope);
-        
         global.set(
-            v8::String::new(&mut handle_scope, "filter").unwrap().into(),
-            FunctionTemplate::new(&mut handle_scope, QueryEngine::filter).into(),
+            v8::String::new(isolate_scope, "filter").unwrap().into(),
+            FunctionTemplate::new(isolate_scope, QueryEngine::filter).into(),
         );
 
-        let context = Context::new(&mut handle_scope,
+        let context = Context::new(
+            isolate_scope,
             v8::ContextOptions {
-              global_template: Some(global),
-              ..Default::default()
-            },);
+                global_template: Some(global),
+                ..Default::default()
+            },
+        );
 
-        let scope = &mut ContextScope::new(&mut handle_scope, context);
+        let mut context_scope = ContextScope::new(isolate_scope, context);
 
-        let code = fs::read_to_string("./script.js").expect("failed to convert Rust string to javascript string");
+        let code = fs::read_to_string("./script.js")
+            .expect("failed to convert Rust string to javascript string");
 
-        let code = v8::String::new(scope, &code).unwrap();
-        println!("javascript code: {}", code.to_rust_string_lossy(scope));
+        let code = v8::String::new(&mut context_scope, &code).unwrap();
 
-        let script = Script::compile(scope, code, None).unwrap();
+        println!(
+            "javascript code: {}",
+            code.to_rust_string_lossy(&mut context_scope)
+        );
 
-        script.run(scope);
+        let script = Script::compile(&mut context_scope, code, None).unwrap();
 
-        let global = context.global(scope);
+        script.run(&mut context_scope);
 
+        return QueryEngine {
+            context,
+            context_scope,
+        };
+    }
+
+    pub fn call(&mut self, name: &str) {
+        let global =  self.context.global(&mut self.context_scope);
+  
         // getting the javascript function whose name is function_name parameter
-        let function_name_string = v8::String::new(scope, "name")
+        let function_name_string = v8::String::new(&mut self.context_scope, &name)
             .expect("failed to convert Rust string to javascript string");
 
         let function = global
-            .get(scope, function_name_string.into())
-            .expect(&*format!("could not find function {}", "name"));
+            .get(&mut self.context_scope, function_name_string.into())
+            .expect(&*format!("could not find function {}", name));
 
         let function: v8::Local<v8::Function> = v8::Local::cast(function);
         // call that function with arguments
-        let recv = v8::Integer::new(scope, 2).into();
+        let recv = v8::Integer::new(&mut self.context_scope, 2).into();
 
-        function.call(scope, recv, &[]);
-
-        Self {
-        }
+        function.call(&mut self.context_scope, recv, &[]);
     }
 
     fn filter(scope: &mut HandleScope, args: FunctionCallbackArguments, mut _retval: ReturnValue) {
