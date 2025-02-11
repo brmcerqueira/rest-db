23bfba1362ca0fc40f1c4ee5f23d18a0ac5a2b99
Bruno <brmcerqueira@gmail.com>
2025-02-11 10:26:42 -0300

    Adjust

---
+++ src/stages/mod.rs
@@ -0,0 +1,18 @@
+pub mod filter; 
+
+use v8::{FunctionTemplate, HandleScope, Local, ObjectTemplate};
+
+macro_rules! global {
+    ($([$name:expr, $cb:expr]),*) => {
+        pub fn global_functions<'s>(scope: &mut HandleScope<'s, ()>, global: Local<'s, ObjectTemplate>) {
+            $(
+                global.set(
+                    v8::String::new(scope, $name).unwrap().into(),
+                    FunctionTemplate::new(scope, $cb).into(),
+                );
+            )*
+        }
+    };
+}
+
+global!(["filter", filter::filter]);
