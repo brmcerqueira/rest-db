ed4b6afa00d54cb134a06f165ee71d684c109e3b
Bruno <brmcerqueira@gmail.com>
2025-02-11 09:17:00 -0300

    Adjust

--- src/stages.rs
+++ src/stages.rs
@@ -1,17 +1,17 @@
 use v8::{FunctionCallbackArguments, FunctionTemplate, HandleScope, Local, ObjectTemplate, ReturnValue};
 
 macro_rules! set_global_function {
-    ($global:expr, $scope:expr, $name:expr, $cb:expr) => {{
-        $global.set(
-            v8::String::new($scope, $name).unwrap().into(),
-            FunctionTemplate::new($scope, $cb).into(),
-        );
-    }};
+    ($name:expr, $cb:expr) => {
+        pub fn global_functions<'s>(scope: &mut HandleScope<'s, ()>, global: Local<'s, ObjectTemplate>) {
+            global.set(
+                v8::String::new(scope, $name).unwrap().into(),
+                FunctionTemplate::new(scope, $cb).into(),
+            );
+        }
+    };
 }
 
-pub fn global_functions<'s>(scope: &mut HandleScope<'s, ()>, global: Local<'s, ObjectTemplate>) {
-    set_global_function!(global, scope, "filter", filter);
-}
+set_global_function!("filter", filter);
 
 fn filter(scope: &mut HandleScope, args: FunctionCallbackArguments, mut _retval: ReturnValue) {
     let message = args
