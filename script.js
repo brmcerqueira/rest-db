0e71e171be09ded6ab1b521f22d5e0d0cf490a7d
Bruno <brmcerqueira@gmail.com>
2025-02-12 19:12:32 -0300

    Adjust

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function name(args) {
     $collection("user");
     $filter(user => user.name.includes(args.text));
     $lookup("user", "test", (l, r) => l.$id == r.$id);
+    $lookup("user", (item, result) => item.test2 = result, (l, r) => l.$id == r.$id);
 }
