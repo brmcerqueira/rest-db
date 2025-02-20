379d659b8e48b4773f24326abe71c9d7644bc3af
Bruno <brmcerqueira@gmail.com>
2025-02-19 23:31:12 -0300

    Adjust

--- src/stages/collection.rs
+++ src/stages/collection.rs
@@ -1,15 +1,19 @@
 use crate::local_array_extension::LocalArrayExtension;
-use crate::utils::out_array;
+use crate::utils::{out_array, try_or_throw};
 use v8::{FunctionCallbackArguments, HandleScope, ReturnValue};
 
-pub fn collection(scope: &mut HandleScope, args: FunctionCallbackArguments, _: ReturnValue) {
-    let array = out_array(&args).unwrap();
+pub fn collection(root_scope: &mut HandleScope, args: FunctionCallbackArguments, _: ReturnValue) {
+    try_or_throw(root_scope, |scope| {
+        let out = out_array(&args)?;
 
-    let collection = args
-        .get(0)
-        .to_string(scope)
-        .unwrap()
-        .to_rust_string_lossy(scope);
+        let collection = args
+            .get(0)
+            .to_string(scope)
+            .ok_or("can't create collection name")?
+            .to_rust_string_lossy(scope);
 
-    array.collection_load(scope, collection);
+        out.collection_load(scope, collection);
+
+        Ok(())
+    });
 }
